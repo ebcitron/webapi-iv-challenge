7a25afada29399d22d5f58221f7c81677dbdd7c8
Eli Citron <ebcitron@gmail.com>
2019-05-09 18:24:37 -0500

    Finishing and Deploying yesterdays project

--- server.js
+++ server.js
@@ -1,31 +1,22 @@
-// Pull in posts/users and set up express 
-const express = require('express');
-const helmet = require('helmet');
+// Pull in posts/users and set up express
+const express = require("express");
+const helmet = require("helmet");
 
-const postsRouter = require('./data/helpers/postsRouter');
-const usersRouter = require('./data/helpers/usersRouter');
+const postsRouter = require("./data/helpers/postsRouter");
+const usersRouter = require("./data/helpers/usersRouter");
 
-
-
-const server = express();;
+const server = express();
 server.use(express.json());
 server.use(helmet());
 
-
-
-
 // write name case middleware
 
-
 //set up user endpoints
-server.use('/api/users', usersRouter);
-server.use('/api/posts', postsRouter);
+server.use("/api/users", usersRouter);
+server.use("/api/posts", postsRouter);
 
-
-
-
-server.get('/', (req, res) => {
-    res.send(`\n*** You Are Now Listening To Localhost:${PORT} *** \n`);
+server.get("/", (req, res) => {
+  res.send("Welcome to my app");
 });
 
 module.exports = server;
