68453b8ea0bf7bd358b85178095dbc54bb834740
Eli Citron <ebcitron@gmail.com>
2019-05-09 18:10:40 -0500

    Finishing and Deploying yesterdays project

--- data/helpers/usersRouter.js
+++ data/helpers/usersRouter.js
@@ -32,7 +32,6 @@ usersRouter.get('/:id/posts', async (req, res) => {
         res.status(500).json({ message: "Error retrieving users"});
     }
 });
-//Get Posts from User
 
 
 //Add a User
@@ -47,9 +46,14 @@ usersRouter.post('/', upName, async(req,res) => {
     });
 
 //Delete a user
-// usersRouter.delete('/:id', (req, res) => {
-//     const userID = req.params.id;
-//     res.status(200).json({})
+    usersRouter.delete('/:id', async (req, res) => {
+        try{
+        const users = await Users.remove(req.params.id);
+    res.status(200).json({message: "User Removed", users});
+        } catch (error) {
+            res.status(500).json({message: "Error Deleting User"});
+        }
+    });
 
 
 //Edit a user
