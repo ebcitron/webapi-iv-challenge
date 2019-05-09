055732f0074810b0c820e96ce21f83097edfa716
Eli Citron <ebcitron@gmail.com>
2019-05-09 18:17:29 -0500

    Finishing and Deploying yesterdays project

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // code away!
+require('dotenv').config();
 const server = require('./server.js');
 
-const PORT = 6660;
+const PORT = process.env.PORT || 6660;
 
 server.listen(PORT, () => {
     console.log(`\n***\n Listening to http://localhost:${PORT}\n*** \n`);
